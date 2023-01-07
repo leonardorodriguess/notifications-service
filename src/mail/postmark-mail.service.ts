1f8544009ad745bee621290ee6e18c5686664fa4
Leonardo Rodrigues <leo-feitosa@live.com>
2023-01-07 18:20:47 -0300

    refactor: Modifica organização e não utilizados

--- src/mail/postmark-mail.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { MailService } from './mail.service';
-
-@Injectable()
-export default class PostmarkMailServive implements MailService {
-  sendEmail(): string {
-    return 'Postmark Mail';
-  }
-}
