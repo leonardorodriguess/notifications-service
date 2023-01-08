2ad7cac1f63e4393463ebfc067dcf760f0a3a13e
Leonardo Rodrigues <leo-feitosa@live.com>
2023-01-08 16:42:10 -0300

    refactor: organiza pasta de infra

--- src/infra/http/controller/app.controller.ts
+++ src/infra/http/controller/app.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
 import { randomUUID } from 'crypto';
-import { CreateNotificationBody } from './create-notification-body';
-import { PrismaService } from './prisma.service';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { PrismaService } from '../../prisma.service';
 
 @Controller('notifications')
 export class AppController {
